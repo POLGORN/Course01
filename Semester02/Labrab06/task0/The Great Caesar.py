614caeeafa91475f307b628fdda3018ca6fe14e1
Егор Полежаев <71223249+POLGORN@users.noreply.github.com>
2021-04-01 12:50:10 +0500

    Create The Great Caesar.py

---
+++ Semester02/Labrab06/task0/The Great Caesar.py
@@ -0,0 +1,31 @@
+def code(alph, line, key):
+    return ([[alph[x + key] for x in range(len(alph)) if line[i] == alph[x]] for i in range(len(line))])
+
+
+def cleaner(one):
+    clean_code = ''
+    for lst in [one[y] for y in range(len(one))]:
+        for sm in lst:
+            clean_code += sm
+    return clean_code
+
+
+rus = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+nums = '1234567890'
+smbs = ':; !&-+=()*/.,'
+alph = rus + rus.upper() + nums + smbs
+
+line = 'Опачки Попачки'
+
+key = 4
+
+print(cleaner(code(alph, line, key)))
+
+# Более читаемая версия первой функции(если так вообще можно выразиться)
+# def code(alph, line, key):
+#     return([
+#         [alph[x + key] 
+#         for x in range(len(alph)) if line[i] == alph[x]
+#         ]
+#         for i in range(len(line))
+#         ])
