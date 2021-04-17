5b524eea782438ee32d64113e8d703db878e4015
Егор Полежаев <71223249+POLGORN@users.noreply.github.com>
2021-04-17 23:55:50 +0500

    Create Encrypt_in_Image.py

---
+++ Semester02/Labrab06/task1/Programs/Encrypt_in_Image.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageDraw # Нужна для работы с картинкой
+from random import randint # Нужна для выбора случайного числа
+
+def Encrypt_in_Image():
+    keys = [] # Здесь будут записаны координаты пикселей в которых шифровали текст
+    img = Image.open(input('В какую картинку изволите шифровать? -> ')) # Вводим картинку
+    draw = ImageDraw.Draw(img) # Создаём обьект для рисования
+    width = img.size[0] # Ширина
+    height = img.size[1] # Высота
+    pix = img.load() # Загружаем "пиксельные данные"
+    f = open('keys.txt', 'w') # Создаём текстовый файл где будут ключи
+
+    for elem in ([ord(elem) for elem in input('Что за секрет? -> ')]): # Этим циклом мы кодируем каждый символ
+        key = (randint(1, width), randint(1, height)) # Сначала наугад ткнем пальцем на пиксель картинки
+        g, b = pix[key][1:3] # Узнаем его зелёный и синий параметр 
+        draw.point(key, (elem, g, b)) # Заменим его красный элемент на ord(elem)
+        f.write(str(key)+'\n') # Напишем координаты пикселя который только что использовали
+
+    img.save('Secret.png', 'PNG') # Сохраним
+    f.close() # Закроем
+
+
+Encrypt_in_Image() # Запустим
